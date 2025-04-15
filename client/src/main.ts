0ff1843988be162f24fcb56967b3823ca85e4eb2
Prosper Matkovic <prosper.matkovic@gmail.com>
2025-04-15 21:41:30 +0200

    Hide console messages in production

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { environment } from 'src/environments/environment';
 
+if (environment.production) {
+  window.console.log = () => { }
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
