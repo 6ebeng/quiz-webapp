4a4d10801ba2548840daf2bc036d9b623fa53c6f
6ebeng <shebeng.devs@gmail.com>
2025-05-16 18:28:59 +0300

    Fix asset

--- server/server.js
+++ server/server.js
@@ -4,86 +4,83 @@ const path = require('path');
 const morgan = require('morgan');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const {MongoClient, ObjectId} = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const requireAuth = require('./app/routes/common/requireAuth');
 const checkRole = require('./app/routes/common/checkRole');
-require('dotenv').config({ path: `.env.${process.env.NODE_ENV}`});
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
 
 const port = process.env.PORT;
 
 async function init() {
-
-    try {
-        const dbClient = new MongoClient(process.env.DB_CONNECTION_STRING);
-        await dbClient.connect();
-        const database = dbClient.db('quizdb');
-        initServer(database);
-
-    } catch (err) {
-        console.error('Error connecting to database', err);
-    }
-};
+	try {
+		const dbClient = new MongoClient(process.env.DB_CONNECTION_STRING);
+		await dbClient.connect();
+		const database = dbClient.db('quizdb');
+		initServer(database);
+	} catch (err) {
+		console.error('Error connecting to database', err);
+	}
+}
 
 function initServer(database) {
+	app.use(morgan('dev'));
+	app.use(express.static(path.join(__dirname, '/public/dist')));
 
-    app.use(morgan('dev'));
-    app.use(express.static(path.join(__dirname, '/public/dist')));
+	if (process.env.NODE_ENV != 'production') {
+		app.use((req, res, next) => {
+			res.setHeader('Access-Control-Allow-Origin', '*');
+			res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+			res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-type, Authorization');
+			if (req.method === 'OPTIONS') {
+				return res.status(200).end();
+			}
+			next();
+		});
+	}
 
-    if (process.env.NODE_ENV != 'production') {
-        
-        app.use((req, res, next) => {
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-type, Authorization');
-            if (req.method === "OPTIONS") {
-                return res.status(200).end();
-            }
-            next();
-        });
-    }
+	app.use(express.urlencoded({ extended: true }));
+	app.use(express.json());
 
+	app.use('/assets', express.static(path.join(__dirname, 'public/app/assets')));
 
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.json());
+	const authRouter = require('./app/routes/auth/auth')(express, database, requireAuth, jwt, bcrypt);
+	app.use('/auth', authRouter);
+	const userRouter = require('./app/routes/api/users')(express, database, ObjectId, requireAuth, checkRole, bcrypt, path);
+	app.use('/api/users', userRouter);
 
-    const authRouter = require('./app/routes/auth/auth')(express, database, requireAuth, jwt, bcrypt);
-    app.use('/auth', authRouter);
-    const userRouter = require('./app/routes/api/users')(express, database, ObjectId, requireAuth, checkRole, bcrypt, path);
-    app.use('/api/users', userRouter);
+	const quizRouter = require('./app/routes/api/quizzes')(express, database, ObjectId, requireAuth, checkRole);
+	app.use('/api/quizzes', quizRouter);
+	const quizResultRouter = require('./app/routes/api/quizresults')(express, database, ObjectId, requireAuth, checkRole);
+	app.use('/api/quiz-results', quizResultRouter);
+	const categoryRouter = require('./app/routes/api/categories')(express, database, ObjectId, requireAuth, checkRole);
+	app.use('/api/categories', categoryRouter);
+	const questionRouter = require('./app/routes/api/questions')(express, database, ObjectId, requireAuth, checkRole);
+	app.use('/api/questions', questionRouter);
+	const assignedQuestionRouter = require('./app/routes/api/assignedquestions')(express, database, ObjectId, requireAuth, checkRole);
+	app.use('/api/assigned-questions', assignedQuestionRouter);
 
-    const quizRouter = require('./app/routes/api/quizzes')(express, database, ObjectId, requireAuth, checkRole);
-    app.use('/api/quizzes', quizRouter);
-    const quizResultRouter = require('./app/routes/api/quizresults')(express, database, ObjectId, requireAuth, checkRole);
-    app.use('/api/quiz-results', quizResultRouter);
-    const categoryRouter = require('./app/routes/api/categories')(express, database, ObjectId, requireAuth, checkRole);
-    app.use('/api/categories', categoryRouter);
-    const questionRouter = require('./app/routes/api/questions')(express, database, ObjectId, requireAuth, checkRole);
-    app.use('/api/questions', questionRouter);
-    const assignedQuestionRouter = require('./app/routes/api/assignedquestions')(express, database, ObjectId, requireAuth, checkRole);
-    app.use('/api/assigned-questions', assignedQuestionRouter);
+	const openAiRouter = require('./app/routes/openai/openai').openAiApi(express, requireAuth);
+	app.use('/openai', openAiRouter);
 
-    const openAiRouter = require('./app/routes/openai/openai').openAiApi(express, requireAuth);
-    app.use('/openai', openAiRouter);
+	app.get('/api/me', requireAuth, (req, res) => {
+		res.json({ status: 'OK', user: req.decoded });
+	});
 
-    app.get('/api/me', requireAuth, (req, res) => {
-        res.json({status: 'OK', user: req.decoded});
-    });
+	app.get('/api/', (req, res) => {
+		res.status(200).send('Welcome to Quiz API');
+	});
 
-    app.get('/api/', (req, res) => {
-        res.status(200).send('Welcome to Quiz API');
-      }); 
+	app.all('/api/*', (req, res) => {
+		res.status(404).send('API route not found');
+	});
 
-    app.all('/api/*', (req, res) => {
-        res.status(404).send('API route not found');
-    });
+	app.get('*', function (req, res) {
+		res.sendFile(path.join(__dirname, '/public/index.html'));
+	});
 
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, '/public/index.html'));
-    });
+	app.listen(port);
 
-    app.listen(port);
-
-    console.log('Running on port: ' + port);
-};
+	console.log('Running on port: ' + port);
+}
 
 init();
